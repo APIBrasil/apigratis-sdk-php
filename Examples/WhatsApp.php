f155b6df5385837d3f8e12e71b006bc8ab071a83
unknown <jhowjhoe@icloud.com>
2023-03-12 17:17:37 -0300

    fix example

--- Examples/WhatsApp.php
+++ Examples/WhatsApp.php
@@ -1,31 +1,30 @@
-<?phphttps://github.com/jhowbhz/package-apigratis/tree/stable/Examples
+<?php
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once('vendor/autoload.php');
-
 use ApiBrasil\Service;
 
 class Example
 {
-
-    //Send Text
     static function sendText()
     {
-        return Service::WhatsApp("sendText", [
-            
+       
+        $response = Service::WhatsApp("sendText", [
             "Bearer" => "SEU TOKEN AQUI",
             "SecretKey" => "SEU SECRETKEY AQUI",
             "PublicToken" => "SEU PUBLICTOKEN AQUI", 
             "DeviceToken" => "SEU DEVICETOKEN AQUI",
-
             "body" => [
                 "number" => "5531994359434",
-                "text" => "🟢 https://apibrasil.com.br \n isso é um teste, caracteres especiais ? : !!! ççç  R$ 999,50"
+                "text" => "🟢 Bem vindo ao APIBrasil"
             ]
         ]);
+        
+        return $response;
+
     }
 
 }
